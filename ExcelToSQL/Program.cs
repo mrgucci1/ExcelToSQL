8c1c77487231ae32be63780c020dee3e6d363dfd
BuildTools <unconfigured@null.spigotmc.org>
2020-06-06 14:07:51 -0500

    Added process config class

--- ExcelToSQL/Program.cs
+++ ExcelToSQL/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ExcelToSQL
 {
@@ -12,9 +13,37 @@ namespace ExcelToSQL
         {
             //References to Classes
             generateConfig cfg = new generateConfig();
-            //Preform Initial Config setup
-            cfg.genConfigStart();
+            readFiles rf = new readFiles();
+            filterClass fil = new filterClass();
+            processCfg pCfg = new processCfg();
+            //Check if config file exists in current dir
+            string[] files = rf.ProcessDirectory(Directory.GetCurrentDirectory());
+            files = fil.filterNoName(files, ".txt", ".txt");
+            //if any file contains name: cfg then cfg exists, process config
+            if (files.Any(s => s.Contains("cfg")))
+            {
+                string[] values = pCfg.processCfgStart(files[0]);
+                for (int i = 0; i < values.Length; i++)
+                {
+                    Console.WriteLine(values[i]);
+                }
+                Console.WriteLine(pCfg.startingNumF);
+                for (int i = 0; i < pCfg.numValuesF.Length; i++)
+                {
+                    Console.WriteLine(pCfg.numValuesF[i]);
+                }
+                for (int i = 0; i < pCfg.dateValuesF.Length; i++)
+                {
+                    Console.WriteLine(pCfg.dateValuesF[i]);
+                }
+                Console.ReadKey();
 
+            }
+            else
+            {
+                //Preform Initial Config setup
+                cfg.genConfigStart();
+            }
         }
     }
 }
---
+++ ExcelToSQL/processCfg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExcelToSQL
+{
+    class processCfg
+    {
+        int[] numValues = new int[200];
+        int[] dateNums = new int[200];
+        int startingNum = 0;
+        public string[] processCfgStart(string path)
+        {
+            int counter = 1;
+            string[] values = new string[200];
+
+            var lines = File.ReadAllLines(path);
+            //Second line is tablename
+            values[0] = lines[1].ToString();
+            //Third Line is Table Header
+            values[1] = lines[2].ToString();
+            //Forth line is starting num
+            startingNum = Convert.ToInt32(lines[3]);
+            //Fifth line is number of numValues
+            numValues = new int[Convert.ToInt32(lines[4])];
+            for (int i = 0; i < Convert.ToInt32(lines[4]) ;i++)
+            {
+                numValues[i] = Convert.ToInt32(lines[5 + i]);
+                counter++;
+            }
+            //nth line is number of dateValues
+            dateNums = new int[Convert.ToInt32(lines[4 + counter])];
+            for (int i = 0; i < Convert.ToInt32(lines[4 + counter]); i++)
+            {
+                dateNums[i] = Convert.ToInt32(lines[(5+counter) + i]);
+            }
+            numValuesF = numValues;
+            dateValuesF = dateNums;
+            startingNumF = startingNum;
+            values = values.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            return values;
+        }
+
+        public int[] numValuesF { get; set; }
+        public int[] dateValuesF { get; set; }
+        public int startingNumF { get; set; }
+    }
+}
